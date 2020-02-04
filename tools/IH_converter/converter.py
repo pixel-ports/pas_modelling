3d0c2eefca3e1f77db24d3528770836b1e3dd5fb
Erwan SIMON <e.simon@catie.fr>
2020-02-04 18:36:36 +0100

    Pivotement sur les dernières données brutes de l'IH
    Du coup le code de convertisseur ne convient plus du tout. 
    Standby jusqu'à mise au propre des données de l'IH (trop aberrantes pr le moment) ?

--- tools/IH_converter/converter.py
+++ tools/IH_converter/converter.py
@@ -10,9 +10,9 @@ def main(input_filepath, output_filepath):
         "terminal": "default",
         "ships_list": []
     }]
-    for call in data:
-        
-        direction = call["operation"]["value"]
+    for call in data[0]["records"]:
+        print(call)
+        # direction = call["operation"]["value"]
         
         if ( call["%s_berth" % direction]["value"] != None and #devra passer en str(None) lorsque l input sera bien en string
             call["arrival_dock"]["value"] != str(None) and
@@ -22,37 +22,37 @@ def main(input_filepath, output_filepath):
             call["%s_tonnage" % direction]["value"] !=0
         ) :
 
-            if call["IMO"]["value"] not in [ship["ID"] for ship in output_data[0]["ships_list"]]:
-                output_data[0]["ships_list"].append({
-                    "ID": call["IMO"]["value"],
-                    "label": call["name"]["value"],  # Is that unique per ship ?
-                    "stopovers_list": []
-                })
-            ship = next(ship for ship in output_data[0]["ships_list"] if ship["ID"]==call["IMO"]["value"])
+        #     if call["IMO"]["value"] not in [ship["ID"] for ship in output_data[0]["ships_list"]]:
+        #         output_data[0]["ships_list"].append({
+        #             "ID": call["IMO"]["value"],
+        #             "label": call["name"]["value"],  # Is that unique per ship ?
+        #             "stopovers_list": []
+        #         })
+        #     ship = next(ship for ship in output_data[0]["ships_list"] if ship["ID"]==call["IMO"]["value"])
 
-            if call["journeyid"]["value"] not in [stopover["ID"] for stopover in ship["stopovers_list"]]:
-                ship["stopovers_list"].append({
-                    "ID": call["journeyid"]["value"],
-                    "handlings_list": []
-                })
-            stopover = next(stopover for stopover in ship["stopovers_list"] if stopover["ID"]==call["journeyid"]["value"])
+        #     if call["journeyid"]["value"] not in [stopover["ID"] for stopover in ship["stopovers_list"]]:
+        #         ship["stopovers_list"].append({
+        #             "ID": call["journeyid"]["value"],
+        #             "handlings_list": []
+        #         })
+        #     stopover = next(stopover for stopover in ship["stopovers_list"] if stopover["ID"]==call["journeyid"]["value"])
 
-            stopover["handlings_list"].append({
-                                "nature": "cargo",
-                                "dangerous": call["%s_dangerous" % direction]["value"],
-                                "agent": call["%s_agent" % direction]["value"],
-                                "dock": {
-                                    "ID": call["%s_berth" % direction]["value"],
-                                    "ETA": datetime.datetime.strptime(call["arrival_dock"]["value"], "%Y-%m-%dT%H:%M:%S.%fZ").isoformat(),
-                                    "ETD": datetime.datetime.strptime(call["departure_dock"]["value"], "%Y-%m-%dT%H:%M:%S.%fZ").isoformat()
-                                },
-                                "contents": {
-                                    "segment": call["%s_cargo_fiscal_type" % direction]["value"],
-                                    "direction": direction,
-                                    "category": call["%s_cargo_type" % direction]["value"],
-                                    "amount": call["%s_tonnage" % direction]["value"]
-                                }
-                            })
+        #     stopover["handlings_list"].append({
+        #                         "nature": "cargo",
+        #                         "dangerous": call["%s_dangerous" % direction]["value"],
+        #                         "agent": call["%s_agent" % direction]["value"],
+        #                         "dock": {
+        #                             "ID": call["%s_berth" % direction]["value"],
+        #                             "ETA": datetime.datetime.strptime(call["arrival_dock"]["value"], "%Y-%m-%dT%H:%M:%S.%fZ").isoformat(),
+        #                             "ETD": datetime.datetime.strptime(call["departure_dock"]["value"], "%Y-%m-%dT%H:%M:%S.%fZ").isoformat()
+        #                         },
+        #                         "contents": {
+        #                             "segment": call["%s_cargo_fiscal_type" % direction]["value"],
+        #                             "direction": direction,
+        #                             "category": call["%s_cargo_type" % direction]["value"],
+        #                             "amount": call["%s_tonnage" % direction]["value"]
+        #                         }
+        #                     })
 
 
     with open(output_filepath, 'w') as outfile:
