c38993b2ac79e1262440489152b70515c5d9d139
Erwan <e.simon@catie.fr>
2020-02-07 20:15:07 +0100

    Minor update json_checker

--- tools/json_checker/json_checker.py
+++ tools/json_checker/json_checker.py
@@ -3,29 +3,18 @@
 # README
 
 ## Current state
-v0.2.1
+v0.2.2
 
 ## Purpose
 This tool check if a given folder containing identified json are valid against their known json-schema.
 
 ## To do
-Use a conf file instead of hard coded names & paths
+[] Use a conf file instead of hard coded names & paths
+[] Faire une vraie gestion des chemins relatifs entre inputs/X et tools/..
+[] Faire des logs ouune édition direct ds le fichier qui pose problème
+[] Ne pas quitter au premier fichier non présent (mais essayer pr chacun de la liste)
 
-## Use example: 
-NB : si lancé depuis 1_PAS_MODELLING/tools/json_checker avec rules et ressources desactivés:
-python3 json_checker.py \
-    --tested_folderPath="../../inputs/simple_examples/" \
-    --schema_folderPath="./json_schemas/"
 
-==> 
----Processing---
-The tested CARGOES_HANDLING_REQUESTS json file is conform to its json-schema.
-The tested SUPPLYCHAINS json file is conform to its json-schema.
----Results---
-2 json were validated against their schema
-All json files required for PAS Modelling have been succefuly checked.
----Closing---
-=====================================================================
 """
 
 
@@ -33,27 +22,45 @@ All json files required for PAS Modelling have been succefuly checked.
 import argparse
 import json
 import jsonschema
-import os
+from pathlib import Path
 import sys
 
 # PRESET JSON NAME
 jsonNames_list = [
-    "CARGOES_HANDLING_REQUESTS",
+#    "CARGOES_HANDLING_REQUESTS",
  #   "RULES",
     "SUPPLYCHAINS",
 #    "RESOURCES"
 ]
 
 # MAIN
-def main(tested_folderPath, schema_folderPath):
+def main(setTestedPath, setScemasPath):
+
+    # Definition des paths :
+    ''' 
+    Le travail en cours consiste à mieux répondre au cas ou l'user est dans le dossier des json à tester qui l'interesse
+    '''
+    cwd = Path.cwd()
+    if setTestedPath== None :
+        tested_folderPath = cwd
+        print(f"No path given for tested files (use -stf), current working directory will be use: {cwd}")
+    else :
+        tested_folderPath = setTestedPath
+
+    if  setScemasPath== None :
+        schema_folderPath = "../..//tools/json_checker/json_schemas/"
+    else :
+        schema_folderPath = setScemasPath
+
+
+        print(f"No path given for json schema fil (use -ssf), default path will be use: {schema_folderPath}")
     # Check existance des dossiers
-    cwd = os.getcwd()
     validated_json = []
     
     print("\n---Processing---\n")
-    for folder in [tested_folderPath, schema_folderPath] :
-        if not os.path.exists(folder) :
-            print(f"Verify the given folder path: {folder} was not found. \nNote that you are currently in {cwd}")
+    for folder in [("tested files", tested_folderPath), ("validation schema", schema_folderPath)] :
+        if not Path(folder[1]).exists() :
+            print(f"Issue for {folder[0]}. Verify the given folder path: {folder[1]} was not found. \nNote that you are currently in {cwd}")
             print("\n---Closing---\n")
             sys.exit()
             
@@ -61,12 +68,12 @@ def main(tested_folderPath, schema_folderPath):
     for json_i in jsonNames_list :
 
         # Création des nom de fichier 
-        tested_filePath = os.path.join(tested_folderPath, json_i + ".json") #<<Mettre un assert là dessu ? (prévention d'un problème sur les caractères exotiques)
-        schema_filePath = os.path.join(schema_folderPath, json_i + "_schema.json")
+        tested_filePath = Path(tested_folderPath).joinpath(json_i + '.json') #<<Mettre un assert là dessu ? (prévention d'un problème sur les caractères exotiques)
+        schema_filePath = Path(schema_folderPath).joinpath(json_i + '_schema.json')
         
         # Check existance des fichiers   
         for file in [tested_filePath, schema_filePath] :#<<Utiliser un dic pr renvoyer un message d'erreur specifique test/schema
-            if not os.path.exists(file) :
+            if not Path(file).exists() :
                 print(f"Verify the deduced file path: issue on {json_i} \n(expected {file} was not found).")
                 print("\n---Closing---\n")
                 sys.exit()
@@ -96,10 +103,11 @@ def main(tested_folderPath, schema_folderPath):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process executable options.")
     parser.add_argument(
-        "--tested_folderPath", type=str, help="Folder path to the json to test"
+        '-stp', '--setTestedPath', type=str, help="To specify the path to the folder containing the set of json to test. (by default your current foder is use)"
     )
+
     parser.add_argument(
-        "--schema_folderPath", type=str, help="Folder path of the json-schema"
+        '-ssp', '--setScemasPath', type=str, help="To specify the path to the folder containing the reference of json-schema. (by default in tools/json_cheker/json_schemas)"
     )
     args = parser.parse_args()
-    main(args.tested_folderPath, args.schema_folderPath)
+    main(args.setTestedPath, args.setScemasPath)
