a8115932695282fd5ebfd2222911502fd023bc7b
Erwan <e.simon@catie.fr>
2020-10-22 18:13:06 +0200

    Passage de PAS builder en full PAS_instance
    Si pas de PAS_instance passé en argument, la valeur par défaut de l'argument et de prendre le contenu d'un fichier local

--- PAS_builder.py
+++ PAS_builder.py
@@ -14,81 +14,62 @@ def main(PAS_instance) :
 	LOGS = ["==== PAS modeling started  ===="]  #Array pr fils chronologique
 	# PAS_INSTANCE LOADING
 	log_message = None
-	if PAS_instance == "local_PAS_instance":
-		file_path = "./PAS_instance.json" 
-		log_source = f"local file {file_path}"
-		try:
-			with open(file_path) as file :
-				PAS_instance = json.load(file)
-				log_status = "success"
-		except Exception as error:
-			log_status = "failed"
-			log_message = error
-	else :
-		log_source = f"Operational Tools"
-		try:
-			PAS_instance = json.loads(PAS_instance) 
-			log_status = "success" 
-		except Exception as error:
-			log_status = "failed"
-			log_message = error
-	LOGS.append(f"Loading current PAS instance data from {log_source}: {log_status} (message: {log_message})")	
+	try:
+		PAS_instance = json.loads(PAS_instance) 
+		log_status = "success" 
+	except Exception as error:
+		log_status = "failed"
+		log_message = error
+	LOGS.append(f"Loading current PAS instance: {log_status} (message: {log_message})")	
 	
 	# INPUTS LOADING
 	inputs = {} 
-	for input_ in PAS_instance['input']: #TODO ajouter gestion log
+	for input_ in PAS_instance['input']:
 		log_item = input_["name"]
-		if input_["category"] == "forceInput":
-			log_source = f"forced input in PAS_instance"
-			for forced_input in PAS_instance['forceinput']:
-				if input_["name"] == forced_input["name"]:
-					if "collection" in input_["type"]:
-						input_["data"] = [item["_source"]["data"] for item in forced_input['value']['hits']['hits']]	
-					elif "tree" in input_["type"]:
-						input_["data"] = forced_input['value']['hits']['hits'][0]["_source"]["data"]
-					else:
-						pass #TODO ajouter une levée d'erreur pour cas non reconnu
-					log_status = "success"
-		elif input_["category"] == "ih-api":
-			log_source = f"remote (IH)"	
-			input_["data"] = get_IH_data(input_)
-		inputs.update(input_)
-		LOGS.append(f"Loading input {log_item} data from {log_source}: {log_status} (message: {log_message})")
-		
-	
-
-
-		SETTINGS = {input_["name"]:input_ for input_ in PAS_instance['input'] if input_["type"]=="settings"}
-		:
-			if input_["name"]=="settings":
-				if input_["category"] == "forceInput": #FIXME faire le cas "pas en force input"
-					source = f"forced input in PAS_instance"
-					for input_ in PAS_instance['forceinput']:
-						if input_["name"]=="settings":
-							SETTINGS = input_['value']['hits']['hits'][0]['_source']['data']
-							status = "success"	
-				else: 
-					source = f"Information Hub"
-					status = "failed"	
-					pass #FIXME faire le cas "pas en force input", cad que la valeur de SETTINGS est soit à aller chercher ds l'IH, soit à charger à partir d'un fichier de settings par défaut. Probablement en éclatant le module de call à l'IH en une fonction qui peut être appeller ici (et à chaque fois qu'un en a besoin).	
-	except Exception as error:
-			status = "failed"
-			message = error
-	LOGS.append(f"Loading settings data from {source}: {status} (message: {message})")
-	#OTHER COMPONENTS
-	<<idem
-	HANDLINGS = [] #Liste des vessel calls à traiter
-	PORT = {} #Conteneur pour l'ensemble des paramètres décrivant le port
-
-	# APPLICATION DES MODULES DE LA PIPELINE
-	modules_sequence = SETTINGS.keys()
-	for module_i in modules_sequence :
-		
-		#UNPROTECTED RUN
+		log_source = "remote (IH)"
+		success, data = get_IH_data(input_)#FIXME faire la fonction
+		if success:
+			input_["data"]= data
+			inputs.update({input_["name"]:input_})
+		LOGS.append(f"Load input {log_item} data from {log_source}: {success}")
+		if not success:
+			LOGS.append({"error": data})
+	for input_ in PAS_instance['forceinput']: #On le place en second car forceinput est prioritaire (doit écraser en cas de doublon)
+		log_item = input_["name"]
+		log_source = "forced input in PAS_instance"
+		try:
+			if ">collection" in input_["type"]:
+				input_["data"] = [item["_source"]["data"] for item in input_['value']['hits']['hits']]	
+			elif ">tree" in input_["type"]:
+				input_["data"] = input_['value']['hits']['hits'][0]["_source"]["data"]
+			elif ">list" in input_["type"]:
+				input_["data"] = input_['value']['hits']['hits'][0]["_source"]["data"]
+			else:
+				pass #TODO ajouter une levée d'erreur pour cas non reconnu
+			success = True
+			inputs.update({input_["name"]:input_})
+		except Exception as error:
+			success = False
+			data = error 
+		LOGS.append(f"Load input {log_item} data from {log_source}: {success}")
+		if not success:
+			LOGS.append({"error": data})
+	#DISPATCH INPUTS
+	SETTINGS = inputs["settings"]["data"] #{name:content for name, content in inputs.items() if content['type'][0:3]=="set"}
+	HANDLINGS = inputs["vesselCalls"]["data"] #[content["data"] for name, content in inputs.items() if content['name']=='vesselCalls']
+	PORT = {}
+	for para_name, para_content in inputs.items():
+		if para_content['type'][0:3]=="PP>":
+			PORT.update({para_name:{}})
+			for item in para_content["data"]:
+				PORT[para_name].update(item)
+ 
+# APPLICATION DES MODULES DE LA PIPELINE
+	for module_i in SETTINGS["pipeline"] :
+		#UNPROTECTED RUN NB for dev & debug
 		exec(f"import {module_i}")
-		HANDLINGS, PORT, LOGS, SETTINGS = eval(f"{module_i}.main(HANDLINGS, PORT, LOGS, SETTINGS, module_i)")
-		
-		#PROTECTED RUN	
+		HANDLINGS, PORT, LOGS = eval(f"{module_i}.main(HANDLINGS, PORT, LOGS, SETTINGS[module_i], module_i)")
+		#PROTECTED RUN	 NB for prod #FIXME
 		# try : 
 		# 	exec(f"import {module_i}")
 		# except Exception as error:
@@ -101,77 +82,56 @@ def main(PAS_instance) :
 		# 		LOGS.append(f"Failled to run: {module_i}.Error: {error}")
 		# 		export_local_output_file(LOGS, SETTINGS)
 	
-	#CLOSSING
+#CLOSSING
 	LOGS.append(f"==== main  ====")
-	LOGS.append(f"End of pipeline {pipeline_name}.")
-
+	LOGS.append(f"End of the run.")
 	print(f"PAS_builder ended, {len(HANDLINGS)} were processed end-to-end. See logs for details")
-	if "local" in pipeline_name:
-		export_local_output_file(LOGS, HANDLINGS, PORT, SETTINGS, abording= False)
+	export_local_output_file(LOGS, HANDLINGS, PORT, SETTINGS, abording= False) #FIXME debug
 	print(f"=============================================================================")	
-	print(f"PAS builder internal logs: {json.dumps(LOGS, indent=4, default=str)}")
-	
+	print(f"PAS modeling internal logs: {json.dumps(LOGS, indent=4, default=str)}")
 	sys.exit(0)
 #=========================================================================
-def get_IH_data(input_:dict)-> dict:
-	pass #FIXME 
-'''
-	if "collection" in input_["type"]: #on doit rendre une liste de n items
-		data = [document["_source"]["data"] for document in reponse_ES['value']['hits']['hits']]	
-	elif "tree" in input_["type"]:
-		data = reponse_ES['value']['hits']['hits'][0]["_source"]["data"]		
-	return data	
-'''
+def get_IH_data(input_:dict)-> (bool, dict): #FIXME
+	success = False
+	data = None
+	try:
+		# if "collection" in input_["type"]: #on doit rendre une liste de n items
+		# 	data = [document["_source"]["data"] for document in reponse_ES['value']['hits']['hits']]	
+		# elif "tree" in input_["type"]:
+		# 	data = reponse_ES['value']['hits']['hits'][0]["_source"]["data"]
+		success = True	
+	except Exception as error:
+		success = False
+		data = error
+	return success, data
 
 def export_local_output_file(LOGS, HANDLINGS= None, PORT= None, SETTINGS= None, abording= True):
 	LOGS.append(f"PAS modelling closing")
-	
 	export = {
 		"LOGS": LOGS,
 		"ACTIVITIES": HANDLINGS,
 		"PORT'S PARAMETERS": PORT,
 		"SETTINGS": SETTINGS	
 	}
-	
 	with open("./PAS_output.json", 'w') as file:
 		json.dump(export, file, indent=4, default=str)
-	
 	print(f"\n\nLocal PAS builder run detected. PAS, logs & settings exported in {file.name} before closing") 
-
-
 	if abording:
 		sys.exit(1)
 
 # SHELL
 if __name__ == "__main__" :
 	parser = argparse.ArgumentParser(description="Process executable options.")
-
+	#Lecture du fichier local comme valeur par défaut
+	with open("./PAS_instance.json" ) as file :
+		local_PAS_instance = json.dumps(json.load(file))
 	parser.add_argument(
 		"-r", "--PAS_instance",
 		nargs='?', 
-		default= None,
+		default= local_PAS_instance,
 		help="Transmited argument from Operational Tools when calling PAS modelling"
 	)
-	
-	parser.add_argument( #TODO en définitive, devrait être un élément contenu dans PAS_instance, il est gardé de côté (en interne) pr le moment
-		"-p", "--pipeline", 
-		nargs='?',
-		default="energy_consumption_assessment", 
-		help="Name of the pipeline to use (see 'settings.json')."
-	)
-
-	#TODO : option facultatives à implémenter
-	# parser.add_argument(
-	#	 "--settingsPath", default="./settings.json", type=str, help="Path to the json file containing settings"
-	# )
-
-	# parser.add_argument(
-	#	 "-v", "--verbose", default="normal", type=str, help="restricted: only valid records in output.\n normal (default value); balanced output.\n extended: every processing logs insered into output. Use for setting issues identification. May generate trouble for output conversion to graph"
-	# )
-
-
 	args = parser.parse_args()
-
 	main(
 		args.PAS_instance
 	)
