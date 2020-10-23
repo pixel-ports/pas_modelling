95f4b6a5f373ff69536d80fb3d5b8be3ef272d45
Erwan <e.simon@catie.fr>
2020-10-23 12:39:18 +0200

    Updated (ok) supplychain_assignation
    Fonctionne

--- MODULES/supplychain_assignation.py
+++ MODULES/supplychain_assignation.py
@@ -2,97 +2,30 @@
 def main(HANDLINGS, PORT, LOGS, SETTINGS, module_name) :
 	'''
 	handling --> handling + [SC]
-
 	Add to each handling an array of suitable SupplyChains (deduced from parameters "RULES>content_type_list").
 	Optionnal filtration can be enabled into Setting
-
-	Pour chaque handling:
-		Confronter aux profils dans  assignment (note)
-		Prendre SCs de celui avec note max
-		ajouter au rejeté si None
-
 	'''
-	# COMMENT
-	# Un code avait été fait pr les DM mis à jour. Cette base est modifiée (passe en commentaires) pr être compatible avec les anciens DM utilisés pr la GUI
-# TEST & DEBUG
-	handling_ = HANDLINGS[0]
-	assign_ = list(PORT["assignments"].items())[0]
-	profils= assign_[1]["handling_profils"]
-	profil_ = profils[0]
-
-	# #FLATTEN
-	# import collections
-	# def flatten(d, parent_key='', sep='_'):
-	# 	items = []
-	# 	for k, v in d.items():
-	# 		new_key = parent_key + sep + k if parent_key else k
-	# 		if isinstance(v, collections.MutableMapping):
-	# 			items.extend(flatten(v, new_key, sep=sep).items())
-	# 		else:
-	# 			items.append((new_key, v))
-	# 	return dict(items)
-	# flatten(profil)
-
-def score_handling_profil(handling, profil, ponderations):
-	for name, content in profil.items():
-		{
-			"terminals": null,
-			"categories": null,
-			"types": null,
-			"directions": null,
-			"ship": {
-				"ids": null,
-				"categories": null,
-				"capacity": {
-					"min": null,
-					"max": null
-				}
-			},
-			"docks": null,
-			"operators": null,
-			"content": {
-				"agents": null,
-				"amount_max": null,
-				"amount_min": null,
-				"dangerous": false,
-				"categories": null,
-				"types": null
-			}
-		}
-	
-	
-# 	[(score_handling_profil(handling, profil, ponderation), name) for name, profil in PORT['assignments']]
-
-
 #INITIALIZATION
 	LOGS.append(f"==== {module_name}  ====")
 	Valid_items = []
 	Invalide_items = [] 
-	Errors = []
-	# error: { 
-	# 			"type": None,
-	# 			"item": None,				
-	# 			"handling": None,
-	# 			"message": f''
-	# 		}
-
-
+	Errors = [] #template pr un item error: {"type": None,"item": None,"handling": None,"message": f''}
+	nb_handling_start = len(HANDLINGS)
 #PROCESSING
 	for handling in HANDLINGS :
-
 	#ASSIGNATION SC PAR CONTENT TYPE
 	#	RETRIVE ASSIGNMENT
-		Candidat_assignments = PORT["Contents"].get(handling["content_type"],{}).get('Suitable_SCs')
+		Candidat_assignments = PORT["contentType"].get(handling["content_type"],{}).get('suitable_assignations')
 		if Candidat_assignments is None: # len(Candidat_assignments) == 0
 			handling['status'] = ('HS', {
 				"type": "Unknown content type",
 				"item": handling["content_type"],
 				"message": f''
 			})
-		elif len(Candidat_assignments) == 0:
+		elif len(Candidat_assignments) == 0: #TODO oui c'est différents, mais un peu redondant quand même
 			handling['status'] = ('HS',	{
 				"type": "No assignment candidat",
-				"item": PORT["Contents"].get(handling["content_type"],{}).get('Suitable_SCs'),
+				"item": PORT["contentType"].get(handling["content_type"],{}).get('suitable_assignations'),
 				"message": f''
 			})
 	#	TEST ASSIGNMENTS
@@ -106,48 +39,36 @@ def score_handling_profil(handling, profil, ponderations):
 				else:
 					Unvalided_assignment.append(data)
 			if len(Valided_assignment) > 0:
-				handling['status'] = ('OK', None)	
-				handling["assigned_SC_ID"] = Valided_assignment[0]  #On prend uniquement la première SC. NB ce choix est discutable, on pourrait prendre toutes les compatibles pr ouvrir le champs à d'autres modules (prochaine version ?)
-				# Valid_items.append(handling)
+				handling['status'] = ('OK', None) #FIXME ce champs est trompeur
+				handling["assigned_SC_ID"] = Valided_assignment[0]["SC_ID"]  #On prend uniquement la première SC. NB ce choix est discutable, on pourrait prendre toutes les compatibles pr ouvrir le champs à d'autres modules (prochaine version ?)
+				Valid_items.append(handling)
 			else:
 				handling['status'] = ('HS',	{
 				"type": "No valid assignements",
 				"item": "None of the candidat assigments was suitable.",
 				"message": f'Detail: {Unvalided_assignment}'
 			})
-
-
-
 	# ASSIGNIATIONS SC PAR DEFAULT 
-		if SETTINGS["default_SC"]:
-			if len(handling.get("assigned_SC_ID", [])) == 0: 
-				success, data = assign_default_SC(handling, PORT["Contents"])#FIXME non implémenté
-				if success:
-					handling["assigned_SC_ID"] = data
-					handling['status'] = ('OK',	{
-						"type": "Default supplychain assignment",
-						"item": "None of the candidat assigments was suitable.",
-						"message": f'Detail: {Unvalided_assignment}'
-					})
-					
-
-	# 	# REMOVE HANDLING'S SC THAT ARE NOT DEFINED IN PORT["Supplychains"]
-	# 	for sc in handling["Supplychains_IDs"]:
-	# 		if sc not in [supplychain["ID"] for supplychain in PORT["Supplychains"]]:
-	# 			handling["Supplychains_IDs"].remove(sc)
-	# 			if sc not in set(Unretrieved_SCs):
-	# 				Unretrieved_SCs.append(sc)			
-				
-
-	
-
-
-	#CLOTURE
+		if SETTINGS["default_SC"]: #FIXME crash si l'option est activée
+			LOGS.append("User settings bypassed, option desactivated")
+			# if len(handling.get("assigned_SC_ID", [])) == 0: 
+			# 	success, data = assign_default_SC(handling, PORT["contentType"])#FIXME non implémenté
+			# 	if success:
+			# 		handling["assigned_SC_ID"] = data
+			# 		handling['status'] = ('OK',	{
+			# 			"type": "Default supplychain assignment",
+			# 			"item": "None of the candidat assigments was suitable.",
+			# 			"message": f'Detail: {Unvalided_assignment}'
+			# 		})
+	# REMOVE HANDLING'S SC THAT ARE NOT DEFINED IN PORT["Supplychains"]
+		# for sc in handling["Supplychains_IDs"]:
+		# 	if sc not in [supplychain["ID"] for supplychain in PORT["Supplychains"]]:
+		# 		handling["Supplychains_IDs"].remove(sc)
+		# 		if sc not in set(Unretrieved_SCs):#FIXME non implémenté
+		# 			Unretrieved_SCs.append(sc)
+#CLOTURE
 	#	REJET DES HANDLINGS SANS SC
-	Unassigned_handlings = [handling
-			for handling in HANDLINGS
-			if len(handling["assigned_SC_ID"]) == 0
-		]
+	Unassigned_handlings = [handling for handling in HANDLINGS if "assigned_SC_ID" not in handling]  #Garantir la présence du champs et tester sur len, ou tester présence ?
 	if SETTINGS["discart_unassigned"]:
 		for handling in Unassigned_handlings:
 			HANDLINGS.remove(handling) #Attention, fiabilité discutable s'il y a des doublons (mais devraient être en double aussi dans la liste mère)	
@@ -166,10 +87,13 @@ def score_handling_profil(handling, profil, ponderations):
 		Errors_synthesise[error['type']][error['item']]["number of occurences"] = len(Errors_synthesise[error['type']][error['item']]["involved handlings"])
 		# Errors_synthesise['Invalide_handlings'].append(error['handling'])
 	
-	LOGS.append(f"Handlings with assigned SC: {len(Valid_items)} ({len(HANDLINGS)} records where passed, {len(Unassigned_handlings)} records discarded)") #FIXME mauvaise manière de calculer, on ne garantie pas la cohérence des chiffres donnés
+	LOGS.append(f"Handlings with assigned SC: {len(Valid_items)} ({nb_handling_start} records where passed, {len(Unassigned_handlings)} records discarded)") #FIXME mauvaise manière de calculer, on ne garantie pas la cohérence des chiffres donnés
+	if (nb_handling_start != len(Valid_items) + len(Unassigned_handlings)) | (len(Valid_items) != len(HANDLINGS)): 
+		raise Exception('Inconsistency between number handlings ok and not ok') #FIXME
+
 	if len(Unassigned_handlings) > 0:
-		LOGS.append({"Details": Errors_synthesise})
-	return HANDLINGS, PORT, LOGS, SETTINGS
+		LOGS.append({"Details": Errors_synthesise}) #FIXME
+	return HANDLINGS, PORT, LOGS
 
 #=====================================================
 def test_assignment_requirements(handling, assignment_restrictions, settings_restrictions): 
@@ -177,7 +101,7 @@ def test_assignment_requirements(handling, assignment_restrictions, settings_res
 	(intersection of {enabled restrictions in settings} and {existing requirement into assignation})
 	NB: a requirement key not present into assignation description is considered as succefully passed but a requirement key present with an empty value will reject every handling
 	'''#Variante, mettre une clé pour activer/désactivé (en gardant la valeur) pour assignment_restrictions ?
-	sucess = None
+	success = None
 	data = None
 
 	try:
