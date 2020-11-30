0e75c1ccc0a25889f6146240f4f0597c42ed3d8c
Erwan <e.simon@catie.fr>
2020-11-30 12:25:25 +0100

    Update Main Export et display pr run local

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import inputs_loader
 import outputs_exporter
 
 
-def main(PAS_instance:dict) :
+def main(PAS_instance:dict, local_export:bool, display_logs:bool) :
 	'''
 	Pour le pipeline passé en argument, appelle successivement les différents modules, leurs passant successivement l'objet "PAS" qui contient l'entièreté des données afférente au scénario.
 	'''
@@ -35,14 +35,25 @@ def main(PAS_instance:dict) :
 		HANDLINGS, PORT, LOGS = eval(f"{module_i}.main(HANDLINGS, PORT, LOGS, SETTINGS[module_i], module_i)")
 		# 	except Exception as error:
 		# 		LOGS.append(f"Failled to run: {module_i}.Error: {error}")
-	
+
 	#CLOSSING
 	LOGS.append(f"==== ENDING  ====")
-	LOGS.append(f"End of the run. PAS properly generated, {len(HANDLINGS)} were processed end-to-end. See logs for details")
+	LOGS.append(f"End of the run. PAS properly generated, {len(HANDLINGS)} were processed end-to-end. See logs for details") #FIXME faux, on peut ne pas récupérer les info (ex: timeout ES)
 	LOGS.append(f"Exporting outputs to Information Hub")
-	LOGS.append(outputs_exporter.main(export_infos=PAS_instance["output"], LOGS=LOGS, HANDLINGS=HANDLINGS))
-	for item in LOGS:
-		print(item)
+	LOGS = outputs_exporter.main(export_infos=PAS_instance["output"], LOGS=LOGS, HANDLINGS=HANDLINGS)
+	if display_logs:
+		for item in LOGS:
+			print(item)
+	if local_export:
+		export = {
+			'logs': LOGS,
+			"handlings": HANDLINGS
+		}
+		with open("./OUTPUTS/PAS_global_output.json", 'w') as file:
+			json.dump(export, file, indent=4, default=str)
+
+		# if abording:
+		# 	sys.exit(1)
 	sys.exit(0)
 #=========================================================================
 # SHELL
@@ -53,12 +64,26 @@ if __name__ == "__main__" :
 	with open(path) as file :
 		local_PAS_instance = json.dumps(json.load(file))
 	parser.add_argument(
-		"-r", "--PAS_instance",
+		"--PAS_instance",
 		nargs='?', 
 		default= local_PAS_instance,
 		help="Transmited argument from Operational Tools when calling PAS modelling"
 	)
+	parser.add_argument(
+		"--local_export",
+		nargs='?', 
+		default= False,
+		help="Export outputs as json files in ./OUTPUTS"
+	)
+	parser.add_argument(
+		"--display_logs",
+		nargs='?', 
+		default= False,
+		help="Display PAS model logs in consol"
+	)
 	args = parser.parse_args()
 	main(
-		args.PAS_instance
+		args.PAS_instance,
+		args.local_export,
+		args.display_logs
 	)
